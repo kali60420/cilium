62ac104b0d7b02e183e4c721cc8ae5816190df3b
Jianlin Lv <Jianlin.Lv@arm.com>
2020-10-19 13:24:28 +0800

    Travis: debug  travis issue of arm64
    Signed-off-by: Jianlin Lv <Jianlin.Lv@arm.com>

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,8 +4,9 @@ set -o errexit
 
 export CFLAGS="-Werror"
 
+cat /boot/config-`uname -r` |grep -i 48
 make -j 2
-make unit-tests
+#make unit-tests
 
 $HOME/gopath/bin/goveralls -coverprofile=coverage-all.out -service=travis-ci || true
 
